574f0d8104dd7696486fe7860602c7e3ded30297
mizuki.kondou <mizukikuma0513@gmail.com>
2022-11-23 13:15:20 +0000

    kadai-fizzbuzz second commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,18 +1,15 @@
+def fizzbuzz(num)
+  if (num % 3 == 0) && (num % 5 == 0)
+     "FizzBuzz"
+  elsif num % 3 == 0
+     "Fizz"
+  elsif num % 5 == 0
+     "Buzz"
+  else
+    return num.to_s
+  end
+end
 num_max = 100
 (1..num_max).each do |num|
-  def fizzbuzz(num)
-    if num % 3 == 0
-      puts "Fizz"
-    elsif num % 5 == 0
-      puts "Buzz"
-    elsif (num % 3 == 0) && (num % 5 == 0)
-      puts "FizzBuzz"
-    else 
-      puts num
-    end
-  end
   puts fizzbuzz(num)
 end
-
-
-  
